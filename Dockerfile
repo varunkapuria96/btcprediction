de3e364f553ff891e46ef6e51a942a4a6c9d72a6
Varun Kapuria <varunkapuria@arizona.edu>
2024-04-23 20:17:44 -0400

    completed data collection AKS cluster

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use an official Python runtime as the parent image
+FROM python:3.8-slim
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy the dependencies file to the working directory
+COPY requirements.txt .
+
+# Install any dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the content of the local src directory to the working directory
+COPY . .
+
+# Command to run on container start
+CMD [ "python", "./data-collection.py" ]
---
+++ data-collection.py
@@ -0,0 +1,29 @@
+import yfinance as yf
+import datetime
+import pandas as pd
+import os
+
+def fetch_and_store_data(ticker_symbol):
+    # Fetching data from Monday to Friday of the current week
+    today = datetime.datetime.now()
+    start_date = (today - datetime.timedelta(days=today.weekday())).strftime('%Y-%m-%d')
+    end_date = today.strftime('%Y-%m-%d')
+
+    # Fetch the stock data
+    data = yf.download(ticker_symbol, start=start_date, end=end_date)
+    
+    # Define the CSV file path
+    file_path = f"{ticker_symbol}.csv"
+    
+    # Check if the file already exists
+    if os.path.exists(file_path):
+        # If the file exists, append without writing the header
+        data.to_csv(file_path, mode='a', header=False)
+        print(f"Data appended to {file_path}")
+    else:
+        # If the file does not exist, write with the header
+        data.to_csv(file_path, mode='w', header=True)
+
+if __name__ == "__main__":
+    if datetime.datetime.today().weekday() == 4:  # Check if today is Friday
+        fetch_and_store_data('BTC-USD')  # For BTC data
